25b1125be08325f91402e723c112f8dc4561fb87
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-06-25 19:35:59 -0300

    creating path alias

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,11 +1,11 @@
 import { Image, View } from "react-native";
 
-import { styles } from "./styles";
+import { styles } from "@/app/home/styles";
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Image source={require("../../assets/logo.png")} style={styles.logo} />
+      <Image source={require("@/assets/logo.png")} style={styles.logo} />
     </View>
   );
 }
