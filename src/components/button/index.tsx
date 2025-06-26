971874bbfad3afe5f893171945b7f8decd893ce5
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-06-26 18:52:01 -0300

    typing component button

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,14 +1,18 @@
-import { Text, TouchableOpacity } from "react-native";
+import {
+  Text,
+  TouchableOpacity,
+  type TouchableOpacityProps,
+} from "react-native";
 
 import { styles } from "@/components/button/styles";
 
 type ButtonProps = {
   title: string;
-};
+} & TouchableOpacityProps;
 
-export function Button({ title }: ButtonProps) {
+export function Button({ title, ...rest }: ButtonProps) {
   return (
-    <TouchableOpacity activeOpacity={0.5} style={styles.container}>
+    <TouchableOpacity activeOpacity={0.8} style={styles.container} {...rest}>
       <Text style={styles.title}>{title}</Text>
     </TouchableOpacity>
   );
